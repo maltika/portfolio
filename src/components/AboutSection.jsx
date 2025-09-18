cf3d0658cc386285648f2b003814f991b640861c
MB <maltikabn@gmail.com>
2025-09-18 21:43:21 +0700

    Update transcript
    Update transcript

--- src/components/AboutSection.jsx
+++ src/components/AboutSection.jsx
@@ -37,7 +37,7 @@ export const AboutSection = () => {
                             </a>
 
                             <a
-                                href=""
+                                href="./myPDF/Transcript .pdf"
                                 target="_blank"
                                 rel="noopener noreferrer"
                                 className="px-6 py-2 rounded-full border border-primary text-primary hover:bg-primary/10 transition-colors duration-300"
